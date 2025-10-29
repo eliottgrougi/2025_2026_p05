02d94ba68e0645273e36489e92239093e999833e
eliottgrougi <geliott0406@gmail.com>
2025-10-29 12:09:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ def ask_for_client_id ():
     client_id = input ("Ton id client: ")
     while is_not_a_valid_client_id (client_id):
         print ("Ton id est invalide, réessaie.")
-    client_id = reask_for_client_id
+        client_id = reask_for_client_id ()
     return client_id
 
 def reask_for_client_id():
-     while is_not_a_valid_client_id(client_id):
-         client_id = reask_for_client_id
+     return input ("Ton id client: ")
  
 client_id = ask_for_client_id ()
