05c466d6a4d48330dc44e92d4ac5ffd86b8bf67b
Tom <tom.renou2009@icloud.com>
2025-10-29 18:09:35 +0100

    wip

--- Menu.py
+++ Menu.py
@@ -1,6 +1,8 @@
-#retrait
-def menu 
- 
+  #1 = Consulter le solde disponible
+ #2 = Retirer de l'argent
+ #3 = Déposer de l'argent
+ #4 = Quitter le programme
 
+ import quitting_words
 
-
+def quit (q):
