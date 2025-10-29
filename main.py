dc31208836c4a42b51f8db5da864f291f93cc86b
eliottgrougi <geliott0406@gmail.com>
2025-10-29 12:02:59 +0100

    .

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-
+from clients import*
 def is_a_valid_client_id (client_id):
-    return clients ["client_id"]["nom"]["prenom"]["solde"]
+    return client_id in clients
 
 def is_not_a_valid_client_id (client_id):
-    return not (is_a_valid_client_id (client_id))
+    return not is_a_valid_client_id (client_id)
 
 def ask_for_client_id ():
     client_id = input ("Ton id client: ")
     while is_not_a_valid_client_id (client_id):
-        reask_for_client_id ()
+        print ("Ton id est invalide, réessaie.")
+    client_id = reask_for_client_id
     return client_id
-    print (clients)
 
 def reask_for_client_id():
-     client_id = input ("Ton id client: ")
-     return client_id
+     while is_not_a_valid_client_id(client_id):
+         client_id = reask_for_client_id
+ 
 client_id = ask_for_client_id ()
