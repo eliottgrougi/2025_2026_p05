72a778a7334cfe1aba2c733a7e2f48626e1fe629
eliottgrougi <geliott0406@gmail.com>
2025-10-29 12:14:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def is_a_valid_client_id (client_id):
 def is_not_a_valid_client_id (client_id):
     return not is_a_valid_client_id (client_id)
 
+def reask_for_client_id():
+     return input ("Ton id client: ")
+
 def ask_for_client_id ():
     client_id = input ("Ton id client: ")
     while is_not_a_valid_client_id (client_id):
@@ -12,7 +15,7 @@ def ask_for_client_id ():
         client_id = reask_for_client_id ()
     return client_id
 
-def reask_for_client_id():
-     return input ("Ton id client: ")
- 
 client_id = ask_for_client_id ()
+
+print(f"Bienvenue {clients[client_id]['prenom']} {clients[client_id]['nom']} !")
+print(f"Ton solde est de {clients[client_id]['solde']} €.")
