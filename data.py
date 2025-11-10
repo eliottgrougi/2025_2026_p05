43e723b9ecbf04db648aff5ab3ea08df8c5aa7ae
eliottgrougi <geliott0406@gmail.com>
2025-11-10 12:51:45 +0100

    Including json load in data

--- data.py
+++ data.py
@@ -7,12 +7,7 @@ retrait_words = ["r", "R", "retrait", "RETRAIT", "Retrait"]
 
 depot_words = ["d", "D", "depot", "DEPOT", "Depot"]
 
-<<<<<<< Updated upstream
 solde_words = ["s", "S", "SOLDE", "solde", "Solde"]
-=======
-solde_words = ["s", "S", "SOLDE", "solde", "Solde", "3"]
 
-with open("clients.json", "r") as f:
-    clients = json.load(f)
- 
->>>>>>> Stashed changes
+with open ("clients.json", 'r') as f:
+    clients = json.load (f)
