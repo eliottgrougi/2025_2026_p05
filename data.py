0f14469e70657e0e57e8cf4827e99a312b8f11d1
Tom <tom.renou2009@icloud.com>
2025-11-13 10:25:13 +0100

    wip number 1,2,3,4

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
 import json
 
 quitting_words = ["quit", "Quit", "q", "Q", "ciao", \
-                  "bye", "byebye", "au revoir", "over", "quitter", "quitté"]
+                  "bye", "byebye", "au revoir", "over", "quitter", "quitté", "4"]
 
-retrait_words = ["r", "R", "retrait", "RETRAIT", "Retrait"]
+retrait_words = ["r", "R", "retrait", "RETRAIT", "Retrait", "1"]
 
-depot_words = ["d", "D", "depot", "DEPOT", "Depot"]
+depot_words = ["d", "D", "depot", "DEPOT", "Depot","2"]
 
-solde_words = ["s", "S", "SOLDE", "solde", "Solde"]
+solde_words = ["s", "S", "SOLDE", "solde", "Solde", "3"]
 
 with open ("clients.json", 'r') as f:
     clients = json.load (f)
