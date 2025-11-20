049341b6c0dffd3b5329e851bcc30974bc835d8d
Tom <tom.renou2009@icloud.com>
2025-11-20 10:44:06 +0100

    wip

--- depot.py
+++ depot.py
@@ -6,10 +6,10 @@ def ask_given_money():
     Entree : 
     Sortie : Entier (qui a été vérifier)
     """
-    montant = input("Combien veux-tu déposer ? ")
+    montant = input("▌ Combien veux-tu déposer ? ")
     while montant.isdigit() == False :
-        print("Veuillez rentrer un montant valide")
-        montant = input("Combien veux-tu déposer ? ")
+        print("▌ Veuillez rentrer un montant valide")
+        montant = input("▌ Combien veux-tu déposer ? ")
 
     return int(montant)
 
@@ -19,8 +19,8 @@ def depot(client_id):
     montant = ask_given_money()
 
     clients[client_id]['solde'] += montant
-    print(f"Dépôt de {montant} € effectué")
-    print(f"Nouveau solde : {clients[client_id]['solde']} €")
+    print(f"▌ Dépôt de {montant} € effectué")
+    print(f"▌ Nouveau solde : {clients[client_id]['solde']} €")
 
 
     save_clients_dict_in_json_file (clients)
